d212a5e2461ae0426844cb847c87b9a4e73f6a2e
Katrix <katrix97@hotmail.com>
2020-06-01 20:12:59 +0200

    Some version updates

--- webpack.prod.js
+++ webpack.prod.js
@@ -51,9 +51,13 @@ module.exports = (env, options) => {
 				}
 			}),
 			new MiniCssExtractPlugin(),
-			new CopyPlugin([{from: 'src/pages/commands/images', to: 'assets/images/commands'}]),
-			new CopyPlugin([{from: 'src/modernizr-custom.js', to: 'modernizr-custom.js'}]),
-			//new BundleAnalyzerPlugin()
+			new CopyPlugin({
+				patterns: [
+					{from: 'src/pages/commands/images', to: 'assets/images/commands'},
+					{from: 'src/modernizr-custom.js', to: 'modernizr-custom.js'}
+				]
+			}),
+			new BundleAnalyzerPlugin()
 		],
 		optimization: {
 			minimizer: [
