dc1ef285daa3633f72e0575b66cf944b1ba2c300
Katrix <katrix97@hotmail.com>
2020-06-18 00:41:22 +0200

    Didn't mean to remove this

--- src/components/InfoFooter.vue
+++ src/components/InfoFooter.vue
@@ -4,5 +4,9 @@
 			Yukkuricraft and affiliated services are made possible by the generous donations by players like you.
 			Consider supporting us through <a href="https://www.patreon.com/remi_scarlet">Patreon</a>.
 		</p>
+
+		<p>
+			&copy; YukkuriCraft
+		</p>
 	</footer>
 </template>
