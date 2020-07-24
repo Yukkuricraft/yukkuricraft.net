031934c55fe751e9033083f4c95822574ed0cadb
Katrix <katrix97@hotmail.com>
2020-07-24 19:17:27 +0200

    Make image generation more data driven

--- processImages.js
+++ processImages.js
@@ -2,77 +2,67 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 const glob = require('glob');
+const imageOptions = require('./images')
 
 let work = [];
-let backgroundDir = './content/images/backgrounds/';
-let backgroundOutDir = './generated/backgrounds/';
 
-let buildsDir = './content/images/locations/';
-let buildsOutDir = './generated/builds/';
+function processImage(image, globalOptions) {
+	let file = path.parse(path.relative(globalOptions.dir, image));
+	let outputFile = globalOptions.outDir + file.dir + '/' + file.name;
+	fs.mkdirSync(globalOptions.outDir + file.dir, {recursive: true});
 
-let avatarsDir = './content/images/avatars/';
-let avatarsOutDir = './generated/avatars/';
+	function callIfFunction(val) {
+		return typeof val === 'function' ? val(image) : val;
+	}
 
-function processBackground(image) {
-	let file = path.parse(path.relative(backgroundDir, image));
-	let outputFile = backgroundOutDir + file.dir + '/' + file.name;
-	fs.mkdirSync(buildsOutDir + file.dir, {recursive: true});
+	function getQuality(options) {
+		return callIfFunction(options.quality || globalOptions.quality);
+	}
 
-	return [
-		sharp(image).jpeg({quality: 60}).resize(1920).toFile(outputFile + '.jpeg'),
-		sharp(image).webp({quality: 60}).resize(1920).toFile(outputFile + '.webp'),
-		sharp(image).jpeg({quality: 65}).resize(1200).toFile(outputFile + '_lg.jpeg'),
-		sharp(image).webp({quality: 65}).resize(1200).toFile(outputFile + '_lg.webp'),
-		sharp(image).jpeg({quality: 70}).resize(992).toFile(outputFile + '_md.jpeg'),
-		sharp(image).webp({quality: 70}).resize(992).toFile(outputFile + '_md.webp'),
-		sharp(image).jpeg({quality: 75}).resize(768).toFile(outputFile + '_sm.jpeg'),
-		sharp(image).webp({quality: 75}).resize(768).toFile(outputFile + '_sm.webp'),
-		sharp(image).jpeg({quality: 80}).resize(576).toFile(outputFile + '_xs.jpeg'),
-		sharp(image).webp({quality: 80}).resize(576).toFile(outputFile + '_xs.webp'),
-		sharp(image).jpeg({quality: 10}).resize(1920 / 8).toFile(outputFile + '_data.jpeg'),
-		sharp(image).webp({quality: 1}).resize(1920 / 8).toFile(outputFile + '_data.webp'),
-	];
+	function types(options) {
+		let quality = getQuality(options);
+
+		return typeof quality === 'object' ? Object.keys(quality) : [
+			quality === true ? 'png' : 'jpeg',
+			'webp'
+		];
+	}
+
+	function base(type, options) {
+		let base = sharp(image)
+		let quality = getQuality(options);
+
+		if (quality === true) {
+			return type === 'webp' ? base.webp({lossless: true}) : base.png();
+		} else {
+			let imgQuality = callIfFunction(quality[type]) || quality;
+			return type === 'webp' ? base.webp({quality: imgQuality}) : base.jpeg({quality: imgQuality});
+		}
+	}
+
+	if (globalOptions.sizes) {
+		return Object
+			.entries(globalOptions.sizes)
+			.flatMap(([name, options]) => types(options).map(type =>
+				base(type, options)
+					.resize(callIfFunction(options.size) || options)
+					.toFile(outputFile + (name !== '' ? '_' : '') + name + '.' + type)));
+	} else {
+		return types({}).map(type => base(type, {}).toFile(outputFile + '.' + type))
+	}
 }
 
-function processBuild(image) {
-	let file = path.parse(path.relative(buildsDir, image));
-	let outputFile = buildsOutDir + file.dir + '/' + file.name;
-	fs.mkdirSync(buildsOutDir + file.dir, {recursive: true});
+function processFiles(options) {
+	fs.mkdirSync(options.outDir, {recursive: true});
+	let files = glob.sync(options.pattern || (options.dir + '**/*.png'));
 
-	return [
-		sharp(image).jpeg({quality: 60}).resize(1920).toFile(outputFile + '.jpeg'),
-		sharp(image).webp({quality: 60}).resize(1920).toFile(outputFile + '.webp'),
-		sharp(image).jpeg({quality: 50}).resize(1920 / 3).toFile(outputFile + '_thumbnail.jpeg'),
-		sharp(image).webp({quality: 50}).resize(1920 / 3).toFile(outputFile + '_thumbnail.webp'),
-	];
+	work.push(...files.flatMap(image => processImage(image, options)));
 }
 
-function processAvatar(image) {
-	let file = path.parse(path.relative(backgroundDir, image));
-	let outputFile = backgroundOutDir + file.dir + '/' + file.name;
-	fs.mkdirSync(buildsOutDir + file.dir, {recursive: true});
+module.exports = function () {
+	imageOptions.forEach(processFiles)
 
-	return [
-		sharp(image).png().resize(192).toFile(outputFile + '.png'),
-		sharp(image).webp({lossless: true}).resize(192).toFile(outputFile + '.webp'),
-		sharp(image).png().resize(64).toFile(outputFile + '_author.png'),
-		sharp(image).webp({lossless: true}).resize(64).toFile(outputFile + '_author.webp'),
-		sharp(image).png().resize(32).toFile(outputFile + '_icon.png'),
-		sharp(image).webp({lossless: true}).resize(32).toFile(outputFile + '_icon.webp'),
-	];
-}
-
-function processFiles(processor, outDir, pattern) {
-	fs.mkdirSync(outDir, {recursive: true});
-	let files = glob.sync(pattern);
-
-	work.push(...files.flatMap(processor));
-}
-
-module.exports = function() {
-	processFiles(processBackground, backgroundOutDir, backgroundDir + '**/*.png')
-	processFiles(processBuild, buildsOutDir, buildsDir + '**/*.png')
-	processFiles(processAvatar, avatarsOutDir, avatarsDir + '**/*.png')
-
-	Promise.all(work).then(() => console.log('Image processing done')).catch(err => console.error('Image processing failed: ', err))
+	Promise.all(work)
+		.then(() => console.log('Image processing done'))
+		.catch(err => console.error('Image processing failed: ', err))
 }
