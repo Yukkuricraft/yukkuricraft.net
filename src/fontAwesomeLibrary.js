7c4bf4f494b07e4d2efd4c62f0e039919cbbc031
Katrix <katrix97@hotmail.com>
2020-10-31 22:51:46 +0100

    Update deps. We'll save webpack for later

--- src/fontAwesomeLibrary.js
+++ src/fontAwesomeLibrary.js
@@ -1,12 +1,9 @@
 import { library, dom, config } from '@fortawesome/fontawesome-svg-core'
 import { faAlignLeft, faLink, faSpinner, faSignal, faTimes } from '@fortawesome/free-solid-svg-icons'
 
-import css from '@fortawesome/fontawesome-svg-core/styles.css'
+import '@fortawesome/fontawesome-svg-core/styles.css'
 
 library.add(faAlignLeft, faLink, faSpinner, faSignal, faTimes)
-
-// eslint-disable-next-line no-void
-void css
 
 config.autoAddCss = false
 
