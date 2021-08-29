767cdf7f0f5675b15e61f4139efdbacad4499931
remiscarlet <remilia@scarl.et>
2021-08-29 20:39:17 +0000

    Add genso v3 announce + update embeded video

---
+++ content/announcements/Genso3-Trailer-Released.md
@@ -0,0 +1,16 @@
+---
+title: Gensokyo v3 Trailer Released!
+time: '2021-08-29T20:33:00Z'
+poster: Remi
+excerpt: New Genso v3 trailer is live and a reminder that the map is open to the public on our server!
+---
+
+Hey everyone,
+
+As the title says, our Gensokyo v3 ~ The Eternal Dream's trailer has been released.
+
+[Check It Out on Youtube!](https://www.youtube.com/watch?v=FI07wDxl5T8)
+
+Alternatively, the video preview on our front page has also been updated to our Genso v3 trailer.
+
+**Additionally**, just another reminder that our Genso v3 map is open to the public for anyone to explore! Hop on our sever at `mc.yukkuricraft.net` and check it out at `/warp neohakurei` or to visit the new Gap Realm, `/warp neogaprealm`!
--- src/pages/InfoPage.vue
+++ src/pages/InfoPage.vue
@@ -32,8 +32,8 @@
           type="iframe"
           aspect="16by9"
           title="YC trailer"
-          src="https://www.youtube-nocookie.com/embed/L6mD2zj8CGs"
-          srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube-nocookie.com/embed/L6mD2zj8CGs?autoplay=1><img src=https://img.youtube.com/vi/L6mD2zj8CGs/hqdefault.jpg alt='Video 【Trailer】【Touhou Minecraft】Gensokyo ~ The Second Dream v2 Shortened Trailer Edition'><span>▶</span></a>"
+          src="https://www.youtube.com/watch?v=FI07wDxl5T8"
+          srcdoc="<style>*{padding:0;margin:0;overflow:hidden}html,body{height:100%}img,span{position:absolute;width:100%;top:0;bottom:0;margin:auto}span{height:1.5em;text-align:center;font:48px/1.5 sans-serif;color:white;text-shadow:0 0 0.5em black}</style><a href=https://www.youtube-nocookie.com/embed/FI07wDxl5T8?autoplay=1><img src=https://img.youtube.com/vi/FI07wDxl5T8/hqdefault.jpg alt='【Touhou Minecraft】Gensokyo ~ The Eternal Dream 【TraFI07wDxl5T8'><span>▶</span></a>"
           frameborder="0"
           allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
           allowfullscreen
