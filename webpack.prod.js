3235cf957e4985f0f9d32afb5f06fabd758b4fbd
Katrix <katrix97@hotmail.com>
2021-10-30 17:08:04 +0200

    Tons of updates (#30)
    Webpack 5 and many smaller updates
    The prerender plugin we used doesn't work great anymore, so rolling our own

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,62 +1,18 @@
-const path = require('path')
 const merge = require('webpack-merge').merge
-const JSDOM = require('jsdom').JSDOM
 
 const CopyPlugin = require('copy-webpack-plugin')
 const TerserJSPlugin = require('terser-webpack-plugin')
 const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
-const PrerenderSPAPlugin = require('prerender-spa-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 // const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin
 
 const common = require('./webpack.common.js')
-const pages = require('./pages')
-
-const Renderer = PrerenderSPAPlugin.PuppeteerRenderer
-
-function processRendered(content) {
-  let htmlContent = content
-  htmlContent = htmlContent.replace('http://localhost:8000', 'https://yukkuricraft.net')
-
-  const dom = new JSDOM(htmlContent)
-  dom.window.document.children[0].classList.remove('webp', 'webp-alpha', 'webp-animation', 'webp-lossless')
-
-  const scripts = dom.window.document.head.querySelectorAll('script')
-  for (const script of scripts) {
-    script.defer = true
-    dom.window.document.body.appendChild(script)
-  }
-
-  return dom.serialize()
-}
 
 module.exports = (env, options) => {
   return merge(common(env, options), {
     mode: 'production',
     devtool: 'source-map',
     plugins: [
-      new PrerenderSPAPlugin({
-        staticDir: path.join(__dirname, 'dist'),
-        indexPath: path.join(__dirname, 'dist', 'index.html'),
-        routes: pages.map((p) => p.url),
-        renderer: new Renderer({
-          headless: true,
-          renderAfterDocumentEvent: 'render-event',
-          injectProperty: '__PRERENDER_INJECTED',
-          inject: {
-            prerendered: true,
-          },
-        }),
-        postProcess(context) {
-          if (context.route === '/404') {
-            context.outputPath = 'dist/404.html'
-          }
-
-          context.html = processRendered(context.html)
-
-          return context
-        },
-      }),
       new MiniCssExtractPlugin(),
       new CopyPlugin({
         patterns: [
