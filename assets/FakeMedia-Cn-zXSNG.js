44fe4a7b9ea865565fa902c85dc0383d6dbbdbaa
LunaPixu <officialtrxd@gmail.com>
2024-03-14 22:33:24 +0000

    Update gh-pages to output generated at dfc3ffa

---
+++ assets/FakeMedia-Cn-zXSNG.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-C-4mJX9H.js";import{o as t,a as r,j as s,h as o}from"./vendor-CerZGVum.js";const c={},n={class:"d-flex"},d={class:"me-3"};function l(e,i){return t(),r("div",n,[s("div",d,[o(e.$slots,"aside")]),s("div",null,[o(e.$slots,"default")])])}const m=a(c,[["render",l]]);export{m as F};
