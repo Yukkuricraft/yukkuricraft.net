500721571b7465cf0cbba1f8d8674842237c94c0
LunaPixu <officialtrxd@gmail.com>
2024-03-15 22:00:43 +0000

    Added new staff to staff page
    Also tweaked staff members to have singular vs. plural account header

--- src/pages/staff/StaffMember.vue
+++ src/pages/staff/StaffMember.vue
@@ -7,7 +7,7 @@
 
     <p>{{ staffMember.description }}</p>
 
-    <h5>Minecraft accounts</h5>
+    <h5>{{ accountsHeader }}</h5>
     <ul class="list-unstyled">
       <li v-for="mcAccount in staffMember.mcAccounts" :key="mcAccount.uuid">
         <minecraft-account :name="mcAccount.name" :uuid="mcAccount.uuid" fetch-username-from-uuid />
@@ -17,16 +17,24 @@
 </template>
 
 <script setup lang="ts">
-import { type PropType } from 'vue'
+import { type PropType, computed } from 'vue'
 import FakeMedia from '@/components/FakeMedia.vue'
 import StaffAvatar from '@/components/StaffAvatar.vue'
 import { type StaffMemberTpe } from '@/pages/staff/StaffPage.vue'
 import MinecraftAccount from '@/components/MinecraftAccount.vue'
 
-defineProps({
+ const props = defineProps({
   staffMember: {
     type: Object as PropType<StaffMemberTpe>,
     required: true,
   },
+})
+
+const accountsHeader = computed(() => {
+  let header: string = 'Minecraft Account'
+  if (props.staffMember.mcAccounts.length > 1) {
+    header += 's'
+  }
+  return header
 })
 </script>
