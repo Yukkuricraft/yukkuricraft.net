8b4e7795d3360ba2ca40d86a41e4b45847cb9199
Katrix <katrix97@hotmail.com>
2024-03-23 10:32:55 +0000

    Update gh-pages-dev to output generated at 7d7dfbf

---
+++ assets/FakeMedia-aD9y086y.js
@@ -0,0 +1,1 @@
+import{b as a}from"./index-rARm-9aJ.js";import{o as t,a as r,j as s,h as o}from"./vendor-CerZGVum.js";const c={},n={class:"d-flex"},d={class:"me-3"};function l(e,i){return t(),r("div",n,[s("div",d,[o(e.$slots,"aside")]),s("div",null,[o(e.$slots,"default")])])}const m=a(c,[["render",l]]);export{m as F};
