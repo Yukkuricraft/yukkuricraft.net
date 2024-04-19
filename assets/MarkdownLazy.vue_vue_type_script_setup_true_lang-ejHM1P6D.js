cbe9827eaa2084e901cb00df3380d0420a177c95
LunaPixu <officialtrxd@gmail.com>
2024-04-19 17:22:33 +0000

    Update gh-pages to output generated at 5068fe0

---
+++ assets/MarkdownLazy.vue_vue_type_script_setup_true_lang-ejHM1P6D.js
@@ -0,0 +1,7 @@
+function __vite__mapDeps(indexes) {
+  if (!__vite__mapDeps.viteFileDeps) {
+    __vite__mapDeps.viteFileDeps = ["assets/MarkdownNow-Bb8i8jF6.js","assets/vendor_markdown-it-Ctdp8CcC.js","assets/vendor-CerZGVum.js"]
+  }
+  return indexes.map((i) => __vite__mapDeps.viteFileDeps[i])
+}
+import{b as r,_ as i}from"./index-BTx9Zy3V.js";import{r as a,o as t,a as c,e as s,f as _,d as m,c as p,u as d,a7 as f}from"./vendor-CerZGVum.js";const l={};function u(n,e){const o=a("font-awesome-icon");return t(),c("div",null,[s(o,{icon:["fas","spinner"],spin:"",size:"3x"}),_(" Loading")])}const g=r(l,[["render",u]]),x={};function h(n,e){const o=a("font-awesome-icon");return t(),c("div",null,[s(o,{class:"text-danger",icon:["fas","times"]}),_(" Error loading content")])}const w=r(x,[["render",h]]),E=m({__name:"MarkdownLazy",props:{content:{type:String,required:!0},noParagraph:Boolean},setup(n){const e=f({loader:()=>i(()=>import("./MarkdownNow-Bb8i8jF6.js"),__vite__mapDeps([0,1,2])),loadingComponent:g,errorComponent:w});return(o,C)=>(t(),p(d(e),{content:n.content,"no-paragraph":n.noParagraph},null,8,["content","no-paragraph"]))}});export{E as _};
