b099406dac10dd2396e7d4e7601a08e85e577192
remiscarlet <remilia@scarl.et>
2024-09-12 17:13:30 -0400

    Separate version vs client versions

--- src/pages/InfoPageParallax.vue
+++ src/pages/InfoPageParallax.vue
@@ -2,7 +2,9 @@
   <h1 class="title is-size-2 has-text-white">Yukkuricraft</h1>
   <p class="subtitle has-text-white">The largest english Touhou Minecraft server.</p>
   <p class="block">Server IP: mc.yukkuricraft.net</p>
-  <p class="block">MC Version: 1.14 - 1.18</p>
+  <p class="block">MC Server Version: 1.18</p>
+  <p class="block">MC Client Compatible Versions: 1.14 - 1.21</p>
+
 
   <div class="block">
     <router-link class="has-text-white button is-primary" :to="{ name: 'download_genso' }"> Map download </router-link
